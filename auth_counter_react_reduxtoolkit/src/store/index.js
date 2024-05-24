6369818632b7f3cbf9ed209cbd6e190edc9ba8ef
CoolShivas <shivas2710cool00@gmail.com>
2024-05-25 00:51:14 +0530

    Formation of  first createSlice named as counterSlice and pass it to reducer

--- auth_counter_react_reduxtoolkit/src/store/index.js
+++ auth_counter_react_reduxtoolkit/src/store/index.js
@@ -1,5 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const counterConfigStore = configureStore({ reducer: {} });
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { counterValue: 0 },
+  reducers: {
+    increment: (currState, action) => {},
+    decrement: (currState, action) => {},
+  },
+});
+
+const counterConfigStore = configureStore({
+  reducer: {
+    counterNumber: counterSlice.reducer,
+  },
+});
 
 export default counterConfigStore;
