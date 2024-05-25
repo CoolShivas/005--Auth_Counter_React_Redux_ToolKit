e5125c8b1a25a1afde8de9a9858b065ace8ee7af
CoolShivas <shivas2710cool00@gmail.com>
2024-05-25 12:27:45 +0530

    Third createSlice is created with reducer and actions

--- auth_counter_react_reduxtoolkit/src/store/index.js
+++ auth_counter_react_reduxtoolkit/src/store/index.js
@@ -1,12 +1,32 @@
 import counterSlice from "./CounterSlice";
 import togglerSlice from "./TogglerSlice";
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const INITIAL_VALUE = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "Authentication",
+  initialState: INITIAL_VALUE,
+  reducers: {
+    logIn: (currState, action) => {
+      currState.isAuthenticated = true;
+    },
+    logOut: (currState, action) => {
+      currState.isAuthenticated = false;
+    },
+  },
+});
 
 const counterConfigStore = configureStore({
   reducer: {
     counterNumber: counterSlice.reducer,
     hide: togglerSlice.reducer,
+    authen: authSlice.reducer,
   },
 });
+
+export const authActions = authSlice.actions;
 
 export default counterConfigStore;
